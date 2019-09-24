8640ca1fffe04cd86a1c2f451606a0d73ccc08c0
CarltonRagsdale <carltonragsdale@knights.ucf.edu>
2019-09-24 19:29:00 -0400

    2D Array Implemented For Navigation

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,10 +8,12 @@ namespace Zork
         {
 
             Console.WriteLine("Welcome to Zork!");
+            SpawnPlayer("West of House");
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
             {
+                Console.WriteLine(Rooms[Location.Row, Location.Column]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
@@ -27,10 +29,23 @@ namespace Zork
                         break;
 
                     case Commands.NORTH:
+                        outputString = $"You moved {command}.";
+                        Location.Row++;
+                        break;
+
                     case Commands.SOUTH:
+                        outputString = $"You moved {command}.";
+                        Location.Row--;
+                        break;
+
                     case Commands.EAST:
+                        outputString = $"You moved {command}.";
+                        Location.Column++;
+                        break;
+
                     case Commands.WEST:
                         outputString = $"You moved {command}.";
+                        Location.Column--;
                         break;
 
                     default:
@@ -41,7 +56,60 @@ namespace Zork
                 Console.WriteLine(outputString);
             }
         }
-            private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, ignoreCase: true, out Commands result) ? result : Commands.UNKNOWN; //THREE OPERATORS: If the first segment is true, it returns the statement before the : otherwise it returns the statement after the :.
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, ignoreCase: true, out Commands result) ? result : Commands.UNKNOWN; //THREE OPERATORS: If the first segment is true, it returns the statement before the : otherwise it returns the statement after the :.
 
+        //private static readonly List<Commands> Directions = new List<Commands>()
+        //{
+        //    Commands.NORTH,
+        //    Commands.SOUTH,
+        //    Commands.EAST,
+        //    Commands.WEST,
+        //};
+
+
+        private static readonly string[,] Rooms =
+        {
+            {"Rocky Trail", "South of House", "Canyon View" },
+            {"Forest", "West of House", "Behind House" },
+            {"Dense Woods", "Noth of House", "Clearing" }
+        };
+
+        private static (int Row, int Column) Location;
+        private static void SpawnPlayer(string roomName)
+        {
+
+            Location = IndexOf(Rooms, roomName);
+            if ((Location.Row, Location.Column) == (-1, -1))
+            {
+                throw new Exception($"Did not find room: {roomName}");
+            }
+
+        }
+
+        private static (int Row, int Column) IndexOf(string[,] values, string valueToFind)
+        {
+            for (int row = 0; row < Rooms.GetLength(0); row++)
+            {
+                for (int column = 0; column < Rooms.GetLength(1); column++)
+                {
+                    if (valueToFind == values[row, column])
+                    {
+                        
+                        return (row, column);
+                    }
+
+                }
+
+            }
+
+            return (-1, -1);
+        }
     }
+
 }
+
+//Method to print each element of an array
+/*foreach (string room in Rooms)
+{
+    console.WriteLine(room);
+}*/
